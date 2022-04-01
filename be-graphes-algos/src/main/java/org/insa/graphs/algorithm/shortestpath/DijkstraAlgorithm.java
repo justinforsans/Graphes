3ee0c74f511478a4355e7cba9fc0cf6842c6ca66
Colombier Korentin <kcolombi@insa-20705>
2022-04-01 18:03:03 +0200

    Label et début Djikstra

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,16 +1,29 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import org.insa.graphs.model.Graph;
+import org.insa.graphs.model.Label;
+import org.insa.graphs.model.Node;
+
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
         super(data);
     }
 
-    @Override
+    @SuppressWarnings("unused")
+	@Override
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
-        // TODO:
+        
+        Graph graph = data.getGraph();
+        final int nbNodes = graph.size();
+        Label[] labels = new Label[nbNodes];
+        for(int i = 0 ; i<nbNodes; i++) {
+        	labels[i]=new Label(i);
+        }
+        
         return solution;
     }
 
